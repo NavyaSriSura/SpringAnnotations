614faae5a4d483aaec0a51f65600e2928ac8c5fd
NavyaSriSura <navyasura98@gmail.com>
2019-07-12 17:43:32 +0530

    first commit

--- src/main/java/com/stackroute/Configuration1.java
+++ src/main/java/com/stackroute/Configuration1.java
@@ -3,6 +3,7 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import  org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Configuration1 {
@@ -15,11 +16,18 @@ public class Configuration1 {
         actor.setAge(20);
         return actor;
     }
-    @Bean
+    @Bean(name={"movie1","movie2"})
+    @Scope("singleton")
     public Movie movieBean()
     {
-
-
+        Movie movie=new Movie();
+        movie.setActor(actorBean());
+        return movie;
+    }
+    @Bean(name = "movie3")
+    @Scope("prototype")
+    public Movie movie3()
+    {
         Movie movie=new Movie();
         movie.setActor(actorBean());
         return movie;
